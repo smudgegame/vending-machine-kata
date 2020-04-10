6192f21bab6378433bca4867432bd384107a9540
smudgegame <atmargraff@gmail.com>
2020-04-10 08:59:18 -0400

    Initial Commit #3

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,24 +1,7 @@
 public class VendingMachine {
 
-    public static boolean isValid(String coin) {
-        if (coin.equalsIgnoreCase("nickle")) return true;
-        if (coin.equalsIgnoreCase("dime")) return true;
-        return coin.equalsIgnoreCase("quarter");
-    }
 
-    public int insertCoin(String coin) {
-        if (isValid(coin)) {
-            if (coin.equalsIgnoreCase("dime"))
-                return 10;
-            if (coin.equalsIgnoreCase("nickle"))
-                return 5;
-            if (coin.equalsIgnoreCase("quarter"))
-                return 25;
-        }
-        return 0;
-    }
-
-    public String display() {
-        return "INSERT COIN";
+    public boolean isValid(int weight) {
+        return weight==1;
     }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -6,44 +6,9 @@ import static org.junit.Assert.assertTrue;
 public class VendingMachineTest {
 
     @Test
-    public void dimeIsValidCoin() {
+    public void isValidCoin() {
         VendingMachine vendingMachine = new VendingMachine();
-        assertTrue(VendingMachine.isValid("Dime"));
+        assertTrue(vendingMachine.isValid(1));
     }
 
-    @Test
-    public void nickleIsValidCoin() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertTrue(VendingMachine.isValid("Nickle"));
-    }
-
-    @Test
-    public void quarterIsValidCoin() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertTrue(VendingMachine.isValid("Quarter"));
-    }
-
-    @Test
-    public void addDimeValue() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertEquals(10, vendingMachine.insertCoin("Dime"));
-    }
-
-    @Test
-    public void addNickleValue() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertEquals(5, vendingMachine.insertCoin("Nickle"));
-    }
-
-    @Test
-    public void addQuarterValue() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertEquals(25, vendingMachine.insertCoin("Quarter"));
-    }
-
-    @Test
-    public void displaysInsertedCoin() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertEquals("INSERT COIN", vendingMachine.display());
-    }
 }
