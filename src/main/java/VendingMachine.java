d9b0048cd9efb2b8408ea7ed8e67f7774c57f66d
smudgegame <atmargraff@gmail.com>
2020-04-10 09:30:58 -0400

    Display shows value of one inserted coin

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,15 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachine {
 
+    private static final int DIME = 1;
+    private int sum = 0;
+
+    public VendingMachine(int sum) {
+        this.sum = sum;
+    }
+
     public boolean isValid(int weight) {
-        return weight == 1;
+        return weight == DIME;
     }
 
     public int insertCoin(int coinWeight) {
-        if (coinWeight == 1) return 10;
+        if (coinWeight == DIME) {
+            sum = 10;
+            return 10;
+        }
         return 0;
     }
 
     public String display() {
-        return "INSERT COIN";
+        if (sum == 0)
+            return "INSERT COIN";
+        else return "$" + (double) sum / 100;
     }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -5,24 +5,32 @@ import static org.junit.Assert.assertTrue;
 
 public class VendingMachineTest {
 
-    private static final int DIME_WEIGHT = 1;
+    private static final int DIME = 1;
 
     @Test
     public void isValidCoin() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertTrue(vendingMachine.isValid(DIME_WEIGHT));
+        VendingMachine vendingMachine = new VendingMachine(0);
+        assertTrue(vendingMachine.isValid(DIME));
     }
 
     @Test
     public void insertCoin() {
-        VendingMachine vendingMachine = new VendingMachine();
-        assertEquals(10, vendingMachine.insertCoin(DIME_WEIGHT));
+        VendingMachine vendingMachine = new VendingMachine(0);
+        assertEquals(10, vendingMachine.insertCoin(DIME));
     }
 
     @Test
     public void insertCoinDisplay() {
-        VendingMachine vendingMachine = new VendingMachine();
+        VendingMachine vendingMachine = new VendingMachine(0);
         assertEquals("INSERT COIN", vendingMachine.display());
+    }
+
+    @Test
+    public void oneCoinValueDisplayed() {
+        VendingMachine vendingMachine = new VendingMachine(0);
+        vendingMachine.insertCoin(DIME);
+        assertEquals("$0.1",vendingMachine.display());
+        //TODO this should read $0.10
     }
 
 }
