474035d6f2d52a752a5fcc7ddbd490e70c65133d
smudgegame <atmargraff@gmail.com>
2020-04-09 22:11:14 -0400

    Initial Commit, restart

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,45 +1,9 @@
-import java.util.Map;
-
 public class VendingMachine {
 
-    private static final int DIME_VALUE = 10;
-    private static final int PENNY_VALUE = 1;
-    private static final int NICKLE_VALUE = 5;
-    private static final int QUARTER_VALUE = 25;
-    private int sum;
-
-    public VendingMachine(int sum) {
-        this.sum = sum;
-    }
-
-    public String insertCoin(int weight) {
-        if (isCoinValid(weight)) {
-            sum += weightToValue(weight);
-        }
-        return showDisplay(sum);
-    }
-
-    private String showDisplay(int sum) {
-        String display;
-        if (sum == 0) display = "INSERT COIN";
-        else display = "$" + (double) sum / 100;
-        return display;
-    }
-
-    private boolean isCoinValid(int weight) {
-        return weight == 1 || weight == 3 || weight == 4;
-    }
-
-    private int weightToValue(int weight) {
-        if (weight == 1) return DIME_VALUE;
-        if (weight == 2) return PENNY_VALUE;
-        if (weight == 3) return NICKLE_VALUE;
-        if (weight == 4) return QUARTER_VALUE;
-        return 0;
-    }
-
-    public void reset() {
-        sum = 0;
+    public static boolean isValid(String coin) {
+        if(coin.equalsIgnoreCase("dime"))
+            return true;
+        return false;
     }
 
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,57 +1,12 @@
-import org.junit.BeforeClass;
 import org.junit.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class VendingMachineTest {
-    private static final int DIME_WEIGHT = 1;
-    private static final int PENNY_WEIGHT = 2;
-    private static final int NICKLE_WEIGHT = 3;
-    private static final int QUARTER_WEIGHT = 4;
-
-
-    private static VendingMachine vendingMachine;
-
-    @BeforeClass
-    public static void setUp() {
-        Map<Integer, Integer> coinHash = new HashMap<Integer, Integer>();
-
-        vendingMachine = new VendingMachine(0);
-    }
-
 
     @Test
-    public void coinValue() {
-        vendingMachine.reset();
-        assertEquals("$0.1", vendingMachine.insertCoin(1));
+    public void dimeIsValidCoin() {
+        VendingMachine vendingMachine = new VendingMachine();
+        assertEquals(true, VendingMachine.isValid("Dime"));
     }
-
-    @Test
-    public void noCoinDisplay() {
-        vendingMachine.reset();
-        assertEquals("INSERT COIN", vendingMachine.insertCoin(0));
-    }
-
-
-    @Test
-    public void nickleValue() {
-        vendingMachine.reset();
-        assertEquals("$0.05", vendingMachine.insertCoin(3));
-    }
-
-    @Test
-    public void quarterValue() {
-        vendingMachine.reset();
-        assertEquals("$0.25", vendingMachine.insertCoin(4));
-    }
-
-    @Test
-    public void rejectPennies() {
-        vendingMachine.reset();
-        assertEquals("INSERT COIN", vendingMachine.insertCoin(2));
-    }
-
 }
