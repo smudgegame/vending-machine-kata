6bc1d295b9edd4240e698d8802239934f9691723
smudgegame <atmargraff@gmail.com>
2020-04-11 12:12:18 -0400

    Machine gets value of coin

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -5,4 +5,8 @@ public class VendingMachine {
     public boolean isValid(Coin coin) {
         return coin.getWeight() == 1;
     }
+
+    public int getValue(Coin coin) {
+        return 10;
+    }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,14 +1,22 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class VendingMachineTest {
 
     @Test
-    public void insertCoin() {
+    public void validCoin() {
         Coin coin = new Coin(1);
         VendingMachine vendingMachine = new VendingMachine();
         assertTrue(vendingMachine.isValid(coin));
+    }
+
+    @Test
+    public void coinValue() {
+        Coin coin = new Coin(1);
+        VendingMachine vendingMachine = new VendingMachine();
+        assertEquals(10, vendingMachine.getValue(coin));
     }
 
 }
