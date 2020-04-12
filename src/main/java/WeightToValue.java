e17a3aec30733a0eb49e1b179f54fd49d405f689
smudgegame <atmargraff@gmail.com>
2020-04-12 13:36:21 -0400

    Adds new coin value

--- src/main/java/WeightToValue.java
+++ src/main/java/WeightToValue.java
@@ -1,8 +1,20 @@
+import java.util.Map;
+
 public class WeightToValue {
 
     private static final int DIME_VALUE = 10;
+    private static final int NICKEL_VALUE = 5;
+    private static final int DIME_WEIGHT = 1;
+    private static final int NICKEL_WEIGHT = 3;
+    private Map<Integer, Integer> valueMap;
+
+    public WeightToValue(Map<Integer, Integer> valueMap) {
+        this.valueMap = valueMap;
+        valueMap.put(DIME_WEIGHT, DIME_VALUE);
+        valueMap.put(NICKEL_WEIGHT, NICKEL_VALUE);
+    }
 
     public int get(Coin coin) {
-        return DIME_VALUE;
+        return valueMap.getOrDefault(coin.getWeight(), 0);
     }
 }
--- src/test/java/WeightToValueTest.java
+++ src/test/java/WeightToValueTest.java
@@ -1,15 +1,28 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class WeightToValueTest {
 
-    private static final int DIME = 1;
+    private static final int DIME_WEIGHT = 1;
+    private static final int NICKEL_WEIGHT = 3;
 
     @Test
     public void weightToValue() {
-        Coin coin = new Coin(DIME);
-        WeightToValue weightToValue = new WeightToValue();
+        Coin coin = new Coin(DIME_WEIGHT);
+        Map<Integer, Integer> valueMap = new HashMap<>();
+        WeightToValue weightToValue = new WeightToValue(valueMap);
         assertEquals(10, weightToValue.get(coin));
+    }
+
+    @Test
+    public void newCoinValue() {
+        Coin coin = new Coin(NICKEL_WEIGHT);
+        Map<Integer, Integer> valueMap = new HashMap<>();
+        WeightToValue weightToValue = new WeightToValue(valueMap);
+        assertEquals(5, weightToValue.get(coin));
     }
 }
