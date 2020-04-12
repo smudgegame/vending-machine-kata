33ad464043d6c772451baa6adb49fcbaabcd5cbc
smudgegame <atmargraff@gmail.com>
2020-04-12 13:40:15 -0400

    Adds quarters value

--- src/test/java/WeightToValueTest.java
+++ src/test/java/WeightToValueTest.java
@@ -9,6 +9,7 @@ public class WeightToValueTest {
 
     private static final int DIME_WEIGHT = 1;
     private static final int NICKEL_WEIGHT = 3;
+    private static final int QUARTER_WEIGHT = 4;
 
     @Test
     public void weightToValue() {
@@ -25,4 +26,14 @@ public class WeightToValueTest {
         WeightToValue weightToValue = new WeightToValue(valueMap);
         assertEquals(5, weightToValue.get(coin));
     }
+
+    @Test
+    public void quarterValue() {
+        Coin coin = new Coin(QUARTER_WEIGHT);
+        Map<Integer, Integer> valueMap = new HashMap<>();
+        WeightToValue weightToValue = new WeightToValue(valueMap);
+        assertEquals(25, weightToValue.get(coin));
+    }
+
+
 }
