2f52636d492467e51bc806ede2f98011499791ac
smudgegame <atmargraff@gmail.com>
2020-04-14 15:02:36 -0400

    Adds current product to inventory

--- src/main/java/Inventory.java
+++ src/main/java/Inventory.java
@@ -3,12 +3,18 @@ import java.util.Map;
 public class Inventory {
 
     private static final int COLA_PRICE = 100;
-    private final Map<String, Integer> productPrice;
+    private Map<String, Integer> productPrice;
+    private String currentProduct;
 
 
-    public Inventory(Map<String, Integer> productPrice) {
+    public Inventory(Map<String, Integer> productPrice, String currentProduct) {
         this.productPrice = productPrice;
+        this.currentProduct = currentProduct;
         productPrice.put("cola",COLA_PRICE);
+    }
+
+    public String getProduct() {
+        return currentProduct;
     }
 
     public int getPrice(String product) {
--- src/test/java/InventoryTest.java
+++ src/test/java/InventoryTest.java
@@ -9,9 +9,18 @@ public class InventoryTest {
 
     @Test
     public void productPrice() {
+        String product = "";
         Map<String, Integer> productPrice = new HashMap<>();
-        Inventory inventory = new Inventory(productPrice);
+        Inventory inventory = new Inventory(productPrice, product);
         assertEquals(100, inventory.getPrice("cola"));
+    }
+
+    @Test
+    public void getProduct() {
+        String currentProduct = "cola";
+        Map<String, Integer> productPrice = new HashMap<>();
+        Inventory inventory = new Inventory(productPrice, currentProduct);
+        assertEquals("cola", inventory.getProduct());
     }
 
 }
